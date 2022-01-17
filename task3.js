cdb9cfe58c3aef0bd2dbd8a0e07bc92bb458559b
eexxprd <aviantofan@gmail.com>
2022-01-17 16:14:51 +0700

    Perubahan variabel

--- task3.js
+++ task3.js
@@ -1,8 +1,8 @@
 const printSegitiga = 5
 if (typeof printSegitiga === "number"){
-    for(let x=printSegitiga; x>0; x--){
+    for(let i=printSegitiga; i>0; i--){
         let temp = ""
-        for(let y=1; y<=x; y++){
+        for(let j=1; j<=x; j++){
             temp += String(y).concat(" ")
         }
         console.log(temp)
